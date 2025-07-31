9d9287b600dec17f1713450f09e3ea9a349465c0
DFNero <abbyansyahatha@gmail.com>
2025-07-31 09:32:57 +0700

    update terus

---
+++ tugas3.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = 'Abby';
+  String hobby = "main dan makan";
+  String jenis_kelamin = '''laki-laki''';
+  print("hurufname.length);
+  print(hobby.contains('Man'));
+  print(name.toLowerCase());
+  print(name.toUpperCase());
+  print(jenis_kelamin.startsWith('l'));
+  print(jenis_kelamin.endsWith('i'));
+  print(name.toLowerCase().replaceAll('a', 'o'));
+}
