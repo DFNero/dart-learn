f17e1b7610b3be927f8c7f7c0620e8a2a7d9e6a4
DFNero <abbyansyahatha@gmail.com>
2025-08-06 20:21:29 +0700

    text rpg class method

---
+++ textrpg.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+class Profile {
+  String nama = '';
+  int level = 0;
+  int kekayaan = 0;
+
+  void tampilkanProfil() {
+    print("||PROFIL KARAKTER||");
+    print('===================');
+    print('Nama: $nama');
+    print('Level: $level');
+    print('Kekayaan: $kekayaan');
+  }
+}
+
+void main() {
+  Profile profile = Profile(); /*pemahaman nya seperti [className objectName = className();]
+                               * atau kalau belum paham kayak gini versi ku [Obj1 ObjClass = Obj1();]
+                               */
+
+
+  stdout.write('Masukkan nama karakter: ');
+  profile.nama = stdin.readLineSync()!;
+
+  stdout.write('Masukkan level karakter: ');
+  profile.level = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Masukkan mau berapa kekayaan: ');
+  profile.kekayaan = int.parse(stdin.readLineSync()!);
+
+  profile.tampilkanProfil();  
+}
